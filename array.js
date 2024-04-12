1b8e2b69fe2046f180eda161e43426d036bab44e
AlejoRocha <rochaalejo67@gmail.com>
2024-04-12 13:11:46 -0300

    se actualiza

---
+++ array.js
@@ -0,0 +1,24 @@
+//ARRAYS
+const numeros = [1,2,3,4,5]
+const booleano = [true, false]
+const mezcla = [1,2,true,false,"hola"]
+//un objeto dentro de un array 
+const usuario = [
+    {
+        nombre: "alejo",
+        edad: 22
+    },
+
+    {
+        nombre: "jose",
+        edad: 30
+    }
+];
+const productos = ["sillon", "silla", "mesa", "ropero"]
+//accediendo a nuestro array 
+console.log(productos[1])
+//recorriendo un array 
+for (let i=0;i<4;i++){
+    console.log(productos[i])
+}
+
---
+++ metodos-array.js
@@ -0,0 +1,52 @@
+/**********************METODOS***********************/
+productos = ["sillon","silla", "mesa", "ropero"]
+
+
+//.length
+console.log(productos.length)//devuelve el largo del array 
+/*recorro un array sin saber la longitud*/
+for (let i =0; i < productos.length;i++){
+    console.log(productos[i])
+} 
+
+//.push agrega un elemento al final del array, se puede agregar una variable
+//let cama = "cama" ** producto.push(cama)
+productos.push("cama")//agrega el valor que le pase al final
+
+//.unshift agrega un elemento al principio del array 
+productos.unshift("puerta")
+
+//.shift elimina el primer elemento del array 
+productos.shift()
+
+//.pop elimina el ultimo elemento del array 
+productos.pop()
+
+//.splice elimina uno o vario elemento que le pase por indice
+//el 1er parametro es el indice y el 2do es la cantidad de elementos q voy a eliminar dsp del indice 
+productos.splice(1, 1)
+
+//.join genera un string con los elemntos del array 
+//los parametros que le pase al join es como se van a separar los string (puede ser con "coma," un espacio o lo que sea)
+console.log(productos.join())
+console.log(productos.join(", "))
+
+//.concat nos permita unir 2 array en uno 
+//se recomienda crear otro array para unir los demas
+const productos2 = ["puerta", "ventana", "cama"]
+const productoFinal = productos.concat(productos2);
+
+//.slice se genera otro array con una copia de otro array 
+//usa 2 parametros, indice de donde empieza y donde termina. No tiene en cuenta el ultimo elemento 
+const productosComedor = productos2.splice(0, 2)
+/* esto devuelve puerta y ventana, cama no devuelve porq ignora el ultimo elemento*/
+
+//indexof, saber el numero de indice de nuestro elemento
+//le paso el valor y nos devuelve el indice 
+console.log(productos.indexOf("silla"))
+
+//includes, devuelve un booleano si existe o no lo que le pase
+console.log(productos.includes("silla"))
+
+//.reverse invierte el orden del array 
+productos.reverse()
